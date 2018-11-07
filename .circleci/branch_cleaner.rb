6a153b6e8ba70f2633a7576d85e29f84e89f64ab
GO Sueyoshi <sue445@users.noreply.github.com>
2018-11-08 08:05:15 +0900

    Refactor branch_cleaner

--- .circleci/branch_cleaner.rb
+++ .circleci/branch_cleaner.rb
@@ -11,10 +11,10 @@ end
 
 `git for-each-ref --sort='-*committerdate' --format="%(refname:short) %(committerdate:raw)" refs/remotes/origin/smoke_test/`.each_line.map do |line|
   branch_name, unixtime, _ = *line.strip.split(" ")
-  [branch_name.gsub("origin/", ""), unixtime.to_i]
-end.select do |_branch_name, unixtime|
+  [branch_name.gsub("origin/", ""), Time.at(unixtime.to_i)]
+end.select do |_branch_name, committer_at|
   # Remove old branch
-  Time.now - Time.at(unixtime) > HOUR_SEC * 12
-end.each do |branch_name, _unixtime|
+  Time.now - committer_at > HOUR_SEC * 12
+end.each do |branch_name, _committer_at|
   system!("git push --delete origin #{branch_name}")
 end
