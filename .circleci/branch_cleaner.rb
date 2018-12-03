ef8c2efd74e76d57607df86caedc31facf6e95c4
sue445 <sue445@sue445.net>
2018-12-03 23:57:40 +0900

    Ignore branch deleting error
    for branch protection

--- .circleci/branch_cleaner.rb
+++ .circleci/branch_cleaner.rb
@@ -3,12 +3,6 @@ require "time"
 # 1 hour
 HOUR_SEC = 60 * 60
 
-def system!(command)
-  puts command
-  ret = system(command)
-  raise "`#{command}` is failed" unless ret
-end
-
 `git for-each-ref --sort='-*committerdate' --format="%(refname:short) %(committerdate:raw)" refs/remotes/origin/smoke_test/`.each_line.map do |line|
   branch_name, unixtime, _ = *line.strip.split(" ")
   [branch_name.gsub("origin/", ""), Time.at(unixtime.to_i)]
@@ -16,5 +10,5 @@ end.select do |_branch_name, committer_at|
   # Remove old branch
   Time.now - committer_at > HOUR_SEC * 12
 end.each do |branch_name, _committer_at|
-  system!("git push --delete origin #{branch_name}")
+  system "git push --delete origin #{branch_name}"
 end
